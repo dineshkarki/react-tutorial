3a969dd545b48e448fcecd67c23c6d37d554917c
dineshkarki <dineshkumar.attur@gmail.com>
2022-09-19 13:37:56 +0530

    search filter updated

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -5,23 +5,31 @@ class FetchData extends React.Component {
     constructor() {
         super();
         this.state = {
-            posts:[]
+            posts:[],
+            query:''
         }
     }
     componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/posts`,{ params: {_limit: 5} })
+        axios.get(`https://jsonplaceholder.typicode.com/posts`,{ params: {_limit: 8} })
         .then(response => 
             {
                 this.setState({posts:response.data})
             }
         )
     }
+    search = (e) => {
+        this.setState({query: e.target.value});
+    }
     render() {
-        const {posts} = this.state;
+        const {posts, query} = this.state;
         return(
             <div>
-                <h4>Get Data from Server</h4>
-                <p>{posts.map(post => <div>{post.title}</div>)}</p>
+                <h4>Get Data from Server &amp; Search</h4>
+                <h5>Class Component</h5>
+                <div>
+                    <input type="text" placeholder='Search title' onChange={this.search} />
+                </div>
+                <p>{posts.filter((post) => post.title.toLowerCase().includes(query)).map(post => <div>{post.title}</div>)}</p>
             </div>
         )
     }
--- src/components/FunctionAPIfetch.js
+++ src/components/FunctionAPIfetch.js
@@ -3,18 +3,24 @@ import React, {useState, useEffect} from "react";
 
 const FunctionAPIfetch = () => {
     const [items,setItems] = useState([]);
+    const [query, setQuery] = useState('');
     const fetchData = async() => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users', { params: {_limit: 8} });
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users', { params: {_limit: 10} });
         setItems(response.data);
+        console.log(response.data);
     }
     useEffect(() => {
         fetchData();
     },[])
     return(
         <div>
-            <h4>Get Data using useEffect</h4>
+            <h4>Get Data &amp; Search</h4>
+            <h5>Function Component</h5>
+                <div>
+                    <input type="text" placeholder='Search name' onChange={(e) => setQuery(e.target.value)} />
+                </div>
             <p>
-                {items.map(item => <div>{item.name}</div>)}
+                {items.filter((item) => item.name.toLowerCase().includes(query)).map(item => <div>{item.name}</div>)}
             </p>
         </div>
     )
